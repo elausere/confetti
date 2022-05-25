75d9ea749085f67395a9d531c1e4e60687184140
elausere <eausere@ewu.edu>
2022-05-24 19:25:19 -0700

    Add Files from Codepen

---
+++ script.js
@@ -0,0 +1,14 @@
+// https://confettijs.org/confetti.min.js
+
+//instantiate new object
+let confetti = new Confetti('confetti');
+
+//options
+confetti.destroyTarget(false);
+
+confetti.setSize(6);
+confetti.setPower(90);
+confetti.setFade(true);
+confetti.setCount(600);
+
+//options
